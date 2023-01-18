451139fbb66dc43d2dfc4b6ddf86c8990e8c9550
Muhammed Furkan <mfyilmaz.hukuk@gmail.com>
2023-01-18 14:11:19 +0300

    v11.1

--- src/components/00-home/menu/menu.js
+++ src/components/00-home/menu/menu.js
@@ -1,18 +1,140 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 const Menu = () => {
   return (
     <nav>
-        <ul>
-            <li><Link to="/hello-world">Hello World</Link></li>
-            <li><Link to="/hello-react">Hello React</Link></li>
-            <li><Link to="/jsx1">JSX1</Link></li>
-            <li><Link to="/jsx2">JSX2</Link></li>
-            <li><Link to="/jsx3">JSX3</Link></li>
-            <li><Link to="/jsx4">JSX4</Link></li>
-            <li><Link to="/jsx5">JSX5</Link></li>
-        </ul>
+      <ul>
+        <li>
+          <Link to="/hello-world">Hello World</Link>
+        </li>
+        <li>
+          <Link to="/hello-react">Hello React</Link>
+        </li>
+        <li>
+          <Link to="/jsx1">JSX1</Link>
+        </li>
+        <li>
+          <Link to="/jsx2">JSX2</Link>
+        </li>
+        <li>
+          <Link to="/jsx3">JSX3</Link>
+        </li>
+        <li>
+          <Link to="/jsx4">JSX4</Link>
+        </li>
+        <li>
+          <Link to="/jsx5">JSX5</Link>
+        </li>
+        <li>
+          <Link to="/jsx6">JSX6</Link>
+        </li>
+        <li>
+          <Link to="/style1">style1</Link>
+        </li>
+        <li>
+          <Link to="/style2">style2</Link>
+        </li>
+        <li>
+          <Link to="/style3">style3</Link>
+        </li>
+        <li>
+          <Link to="/style4">style4</Link>
+        </li>
+        <li>
+          <Link to="/greetings">greetings</Link>
+        </li>
+        <li>
+          <Link to="/products">products</Link>
+        </li>
+        <li>
+          <Link to="/clock2">clock2</Link>
+        </li>
+        <li>
+          <Link to="/image">image</Link>
+        </li>
+        <li>
+          <Link to="/gallery">gallery</Link>
+        </li>
+        <li>
+          <Link to="/profileCard">profileCard</Link>
+        </li>
+        <li>
+          <Link to="/bootstrapStatic">bootstrapStatic</Link>
+        </li>
+        <li>
+          <Link to="/bootstrapDynamic">bootstrapDynamic</Link>
+        </li>
+        <li>
+          <Link to="/icon">icon</Link>
+        </li>
+        <li>
+          <Link to="/events">events</Link>
+        </li>
+        <li>
+          <Link to="/shop">shop</Link>
+        </li>
+        <li>
+          <Link to="/stateless">stateless</Link>
+        </li>
+        <li>
+          <Link to="/state">state</Link>
+        </li>
+        <li>
+          <Link to="/counter">counter</Link>
+        </li>
+        <li>
+          <Link to="/birthday">birthday</Link>
+        </li>
+        <li>
+          <Link to="/useEffect1">useEffect1</Link>
+        </li>
+        <li>
+          <Link to="/clock3">clock3</Link>
+        </li>
+        <li>
+          <Link to="/useEffect2">useEffect2</Link>
+        </li>
+        <li>
+          <Link to="/countryFilter">countryFilter</Link>
+        </li>
+        <li>
+          <Link to="/useRef">useRef</Link>
+        </li>
+        <li>
+          <Link to="/scroll">scroll</Link>
+        </li>
+        <li>
+          <Link to="/classComp">classComp</Link>
+        </li>
+        <li>
+          <Link to="/userCards">userCards</Link>
+        </li>
+        <li>
+          <Link to="/countries">countries</Link>
+        </li>
+        <li>
+          <Link to="/parentComp">parentComp</Link>
+        </li>
+        <li>
+          <Link to="/form1">form1</Link>
+        </li>
+        <li>
+          <Link to="/form2">form2</Link>
+        </li>
+        <li>
+          <Link to="/form3">form3</Link>
+        </li>
+        <li>
+          <Link to="/form4">form4</Link>
+        </li>
+        <li>
+          <Link to="/form5">form5</Link>
+        </li>
+        <li>
+          <Link to="/form6">form6</Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
-export default Menu
+  );
+};
+export default Menu;
